206496c741bcac8295378a14f0ea640c0f992e33
Glen van de Mosselaer <glenvan@gmail.com>
2023-11-12 11:15:39 -0700

    Add Concurrency UTs and Goroutine Leak Tests
    - Add concurrency UTs
      - Concurrent Store and Delete
      - Concurrent Store and DeleteFunc
        - One to delete only the stored item
        - One to delete according to a match
    - Add `github.com/fortytw2/leaktest` to UTs
      - Test each UT for goroutine leaks (result of Map.Close() not being
        called)

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/glenvan/ttl
 
 go 1.21
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/fortytw2/leaktest v1.3.0
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- map_test.go
+++ map_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/glenvan/ttl"
@@ -18,6 +19,7 @@ type MapTestSuite struct {
 	pruneInterval time.Duration
 	startSize     int
 	sleepTime     time.Duration
+	leakTestFunc  func()
 }
 
 func (s *MapTestSuite) SetupSuite() {
@@ -25,6 +27,14 @@ func (s *MapTestSuite) SetupSuite() {
 	s.pruneInterval = 100 * time.Millisecond
 	s.startSize = 3
 	s.sleepTime = s.maxTTL + 2*s.pruneInterval
+}
+
+func (s *MapTestSuite) SetupTest() {
+	s.leakTestFunc = leaktest.Check(s.T())
+}
+
+func (s *MapTestSuite) TearDownTest() {
+	s.leakTestFunc()
 }
 
 func TestMapTestSuite(t *testing.T) {
@@ -426,5 +436,122 @@ func (s *MapTestSuite) TestRangeTransform() {
 			"Actual %v does not equal expected %v",
 			v,
 			transformedThirdSlice)
+	}
+}
+
+func (s *MapTestSuite) TestConcurrentStoreExpire() {
+	refreshLastAccessOnGet := true
+	tm := ttl.NewMap[uint64, uint64](s.maxTTL, s.startSize, s.pruneInterval, refreshLastAccessOnGet)
+	defer tm.Close()
+
+	startTime := time.Now()
+
+	doneCh := make(chan struct{})
+
+	go func() {
+		var iteration uint64
+		for time.Since(startTime) < s.maxTTL {
+			tm.Store(iteration, iteration)
+			iteration++
+		}
+
+		time.Sleep(s.sleepTime)
+		close(doneCh)
+	}()
+
+	<-doneCh
+
+	s.Zero(tm.Length())
+}
+
+func (s *MapTestSuite) TestConcurrentStoreDelete() {
+	refreshLastAccessOnGet := true
+	tm := ttl.NewMap[int, int](s.maxTTL, s.startSize, s.pruneInterval, refreshLastAccessOnGet)
+	tm.Close() // disable pruning
+
+	startTime := time.Now()
+
+	storedCh := make(chan int, 1000)
+	var iteration int
+
+	go func() {
+		for time.Since(startTime) < s.maxTTL {
+			tm.Store(iteration, iteration)
+			storedCh <- iteration
+			iteration++
+		}
+
+		close(storedCh)
+	}()
+
+	for storedKey := range storedCh {
+		tm.Delete(storedKey)
+	}
+
+	s.Zero(tm.Length())
+	s.Greater(iteration, 0)
+}
+
+func (s *MapTestSuite) TestConcurrentStoreDeleteFuncOne() {
+	refreshLastAccessOnGet := true
+	tm := ttl.NewMap[int, int](s.maxTTL, s.startSize, s.pruneInterval, refreshLastAccessOnGet)
+	tm.Close() // disable pruning
+
+	startTime := time.Now()
+
+	storedCh := make(chan int, 1000)
+	var iteration int
+
+	go func() {
+		for time.Since(startTime) < s.maxTTL {
+			tm.Store(iteration, iteration)
+			storedCh <- iteration
+			iteration++
+		}
+
+		close(storedCh)
+	}()
+
+	for storedKey := range storedCh {
+		tm.DeleteFunc(func(key int, _ int) bool {
+			return key == storedKey
+		})
+	}
+
+	s.Zero(tm.Length())
+	s.Greater(iteration, 0)
+}
+
+func (s *MapTestSuite) TestConcurrentStoreDeleteFuncMatch() {
+	refreshLastAccessOnGet := true
+	tm := ttl.NewMap[int, int](s.maxTTL, s.startSize, s.pruneInterval, refreshLastAccessOnGet)
+	tm.Close() // disable pruning
+
+	startTime := time.Now()
+
+	storedCh := make(chan int, 1000)
+	iteration := 0
+
+	go func() {
+		for time.Since(startTime) < s.maxTTL {
+			tm.Store(iteration+1, iteration+1)
+			storedCh <- iteration + 1
+			iteration++
+		}
+
+		close(storedCh)
+	}()
+
+	for range storedCh {
+		tm.DeleteFunc(func(key int, _ int) bool {
+			// Delete all odd keys
+			return key%2 == 1
+		})
+	}
+
+	if s.Greater(iteration, 0) {
+		// Since we're deleting odd keys this should never be off by one if iteration isn't
+		// an even number
+		s.Equal(iteration/2, tm.Length())
 	}
 }
